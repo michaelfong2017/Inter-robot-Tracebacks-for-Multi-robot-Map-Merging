6a60f7afd7897dd9b9d8f94feafb864d80161622
michaelfong2017 <michaelfong2017@gmail.com>
2023-01-07 19:56:04 +0800

    Subscribe to /tb3*/camera/rgb/image_raw and convert ros image to cv image.

--- src/traceback/include/traceback/traceback.h
+++ src/traceback/include/traceback/traceback.h
@@ -5,6 +5,7 @@
 
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Transform.h>
+#include <sensor_msgs/Image.h>
 #include <map_msgs/OccupancyGridUpdate.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <ros/ros.h>
@@ -21,6 +22,13 @@ namespace traceback
     nav_msgs::OccupancyGrid::ConstPtr readonly_map;
     ros::Subscriber map_sub;
     ros::Subscriber map_updates_sub;
+    std::string robot_namespace; // e.g /tb3_0
+  };
+
+  struct CameraSubscription
+  {
+    sensor_msgs::ImageConstPtr readonly_camera_image;
+    ros::Subscriber camera_sub;
     std::string robot_namespace; // e.g /tb3_0
   };
 
@@ -43,15 +51,23 @@ namespace traceback
     std::string robot_map_updates_topic_;
     std::string robot_namespace_;
 
+    std::string robot_camera_image_topic_;
+    double camera_image_update_rate_;
+
     const tf::TransformListener tf_listener_; ///< @brief Used for transforming
-    double transform_tolerance_;            ///< timeout before transform errors
+    double transform_tolerance_;              ///< timeout before transform errors
 
     // maps robots namespaces to maps. does not own
-    std::unordered_map<std::string, MapSubscription *> robots_;
+    std::unordered_map<std::string, MapSubscription *> robots_to_map_subscriptions_;
     // owns maps -- iterator safe
-    std::forward_list<MapSubscription> subscriptions_;
-    size_t subscriptions_size_;
-    boost::shared_mutex subscriptions_mutex_;
+    std::forward_list<MapSubscription> map_subscriptions_;
+    size_t map_subscriptions_size_;
+    boost::shared_mutex map_subscriptions_mutex_;
+
+    std::unordered_map<std::string, CameraSubscription *> robots_to_camera_subscriptions_;
+    std::forward_list<CameraSubscription> camera_subscriptions_;
+    size_t camera_subscriptions_size_;
+    boost::shared_mutex camera_subscriptions_mutex_;
 
     TransformEstimator transform_estimator_;
     // maps transform indexes to robots namespaces
@@ -62,18 +78,24 @@ namespace traceback
 
     geometry_msgs::Pose getRobotPose(const std::string &global_frame, const std::string &robot_base_frame, const tf::TransformListener &tf_listener, const double &transform_tolerance);
 
+    void receiveUpdatedCameraImage();
+
     void poseEstimation();
 
+    void CameraImageUpdate(const sensor_msgs::ImageConstPtr &msg,
+                           CameraSubscription &subscription);
     void fullMapUpdate(const nav_msgs::OccupancyGrid::ConstPtr &msg,
-                       MapSubscription &map);
+                       MapSubscription &subscription);
 
     void topicSubscribing();
 
     std::string robotNameFromTopic(const std::string &topic);
     bool isRobotMapTopic(const ros::master::TopicInfo &topic);
+    bool isRobotCameraTopic(const ros::master::TopicInfo &topic);
 
     void executeUpdateTargetPoses();
     void executeTopicSubscribing();
+    void executeReceiveUpdatedCameraImage();
     void executePoseEstimation();
   };
 } // namespace traceback
--- src/traceback/src/traceback.cpp
+++ src/traceback/src/traceback.cpp
@@ -2,10 +2,15 @@
 
 #include <thread>
 #include <algorithm>
+#include <regex>
+
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 
 namespace traceback
 {
-  Traceback::Traceback() : subscriptions_size_(0), tf_listener_(ros::Duration(10.0))
+  Traceback::Traceback() : map_subscriptions_size_(0), camera_subscriptions_size_(0), tf_listener_(ros::Duration(10.0))
   {
     ros::NodeHandle private_nh("~");
 
@@ -19,6 +24,9 @@ namespace traceback
     private_nh.param<std::string>("robot_namespace", robot_namespace_, "");
     // transform tolerance is used for all tf transforms here
     private_nh.param("transform_tolerance", transform_tolerance_, 0.3);
+
+    private_nh.param<std::string>("camera_image_topic", robot_camera_image_topic_, "camera/rgb/image_raw"); // Don't use image_raw
+    private_nh.param("camera_image_update_rate", camera_image_update_rate_, 0.1);
   }
 
   void Traceback::updateTargetPoses()
@@ -137,22 +145,28 @@ namespace traceback
     return msg.pose;
   }
 
+  void Traceback::receiveUpdatedCameraImage()
+  {
+    ROS_DEBUG("Receive updated camera image started.");
+  }
+
   void Traceback::poseEstimation()
   {
     ROS_DEBUG("Grid pose estimation started.");
     std::vector<nav_msgs::OccupancyGridConstPtr> grids;
-    grids.reserve(subscriptions_size_);
+    grids.reserve(map_subscriptions_size_);
     transforms_indexes_.clear();
     resolutions_.clear();
     {
-      boost::shared_lock<boost::shared_mutex> lock(subscriptions_mutex_);
+      boost::shared_lock<boost::shared_mutex> lock(map_subscriptions_mutex_);
       size_t i = 0;
-      for (auto &subscription : subscriptions_)
+      for (auto &subscription : map_subscriptions_)
       {
         // In case the map topic is just subscribed and pose estimation is started before
         // receiving the first map update from that topic, this subscription should
         // be skipped to prevent error.
-        if (!subscription.readonly_map) {
+        if (!subscription.readonly_map)
+        {
           ++i;
           continue;
         }
@@ -168,6 +182,27 @@ namespace traceback
                                             confidence_threshold_);
   }
 
+  void Traceback::CameraImageUpdate(const sensor_msgs::ImageConstPtr &msg, CameraSubscription &subscription)
+  {
+    // ROS_DEBUG("received camera image update");
+    cv_bridge::CvImageConstPtr cv_ptr;
+    try
+    {
+      if (sensor_msgs::image_encodings::isColor(msg->encoding))
+        cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
+      else
+        cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8);
+    }
+    catch (cv_bridge::Exception &e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Process cv_ptr->image using OpenCV
+    // ROS_INFO("Process cv_ptr->image using OpenCV");
+  }
+
   void Traceback::fullMapUpdate(const nav_msgs::OccupancyGrid::ConstPtr &msg,
                                 MapSubscription &subscription)
   {
@@ -181,94 +216,170 @@ namespace traceback
 
     subscription.readonly_map = msg;
   }
-  /*
-   * Subcribe to pose and map topics
-   */
+
+  // Subcribe to pose and map topics
   void Traceback::topicSubscribing()
   {
     ros::master::V_TopicInfo topic_infos;
     std::string robot_name;
-    std::string map_topic;
-    std::string map_updates_topic;
 
     ros::master::getTopics(topic_infos);
     // default msg constructor does no properly initialize quaternion
 
     for (const auto &topic : topic_infos)
     {
-      // we check only map topic
-      if (!isRobotMapTopic(topic))
+      if (isRobotMapTopic(topic))
       {
-        continue;
-      }
+        std::string map_topic;
+        std::string map_updates_topic;
 
-      robot_name = robotNameFromTopic(topic.name);
-      if (robots_.count(robot_name))
+        robot_name = robotNameFromTopic(topic.name);
+        if (robots_to_map_subscriptions_.count(robot_name))
+        {
+          // we already know this robot
+          continue;
+        }
+
+        ROS_INFO("(map topic) adding robot [%s] to system", robot_name.c_str());
+        {
+          std::lock_guard<boost::shared_mutex> lock(map_subscriptions_mutex_);
+          map_subscriptions_.emplace_front();
+          ++map_subscriptions_size_;
+        }
+
+        // no locking here. robots_to_map_subscriptions_ are used only in this procedure
+        MapSubscription &subscription = map_subscriptions_.front();
+        robots_to_map_subscriptions_.insert({robot_name, &subscription});
+
+        /* subscribe callbacks */
+        map_topic = ros::names::append(robot_name, robot_map_topic_);
+        // map_updates_topic =
+        //     ros::names::append(robot_name, robot_map_updates_topic_);
+
+        subscription.robot_namespace = robot_name;
+
+        ROS_INFO("Subscribing to MAP topic: %s.", map_topic.c_str());
+        subscription.map_sub = node_.subscribe<nav_msgs::OccupancyGrid>(
+            map_topic, 50,
+            [this, &subscription](const nav_msgs::OccupancyGrid::ConstPtr &msg)
+            {
+              fullMapUpdate(msg, subscription);
+            });
+        // subscription.is_self = robot_name == ros::this_node::getNamespace();
+        // ROS_INFO("subscription.is_self: %s", subscription.is_self ? "true" : "false");
+
+        // ROS_INFO("Subscribing to MAP updates topic: %s.",
+        //          map_updates_topic.c_str());
+        // subscription.map_updates_sub =
+        //     subscription_nh.subscribe<map_msgs::OccupancyGridUpdate>(
+        //         map_updates_topic, 50,
+        //         [this, &subscription](
+        //             const map_msgs::OccupancyGridUpdate::ConstPtr& msg) {
+        //           partialMapUpdate(msg, subscription);
+        //         });
+      }
+      else if (isRobotCameraTopic(topic))
       {
-        // we already know this robot
-        continue;
+        std::string camera_topic;
+
+        robot_name = robotNameFromTopic(topic.name);
+        if (robots_to_camera_subscriptions_.count(robot_name))
+        {
+          // we already know this robot
+          continue;
+        }
+
+        ROS_INFO("(camera topic) adding robot [%s] to system", robot_name.c_str());
+        {
+          std::lock_guard<boost::shared_mutex> lock(camera_subscriptions_mutex_);
+          camera_subscriptions_.emplace_front();
+          ++camera_subscriptions_size_;
+        }
+
+        // no locking here. robots_to_camera_subscriptions_ are used only in this procedure
+        CameraSubscription &subscription = camera_subscriptions_.front();
+        robots_to_camera_subscriptions_.insert({robot_name, &subscription});
+
+        /* subscribe callbacks */
+        camera_topic = ros::names::append(robot_name, robot_camera_image_topic_);
+
+        subscription.robot_namespace = robot_name;
+
+        ROS_INFO("Subscribing to CAMERA topic: %s.", camera_topic.c_str());
+        subscription.camera_sub = node_.subscribe<sensor_msgs::Image>(
+            camera_topic, 50,
+            [this, &subscription](const sensor_msgs::ImageConstPtr &msg)
+            {
+              CameraImageUpdate(msg, subscription);
+            });
       }
-
-      ROS_INFO("adding robot [%s] to system", robot_name.c_str());
-      {
-        std::lock_guard<boost::shared_mutex> lock(subscriptions_mutex_);
-        subscriptions_.emplace_front();
-        ++subscriptions_size_;
-      }
-
-      // no locking here. robots_ are used only in this procedure
-      MapSubscription &subscription = subscriptions_.front();
-      robots_.insert({robot_name, &subscription});
-
-      /* subscribe callbacks */
-      map_topic = ros::names::append(robot_name, robot_map_topic_);
-      // map_updates_topic =
-      //     ros::names::append(robot_name, robot_map_updates_topic_);
-
-      subscription.robot_namespace = robot_name;
-
-      ROS_INFO("Subscribing to MAP topic: %s.", map_topic.c_str());
-      subscription.map_sub = node_.subscribe<nav_msgs::OccupancyGrid>(
-          map_topic, 50,
-          [this, &subscription](const nav_msgs::OccupancyGrid::ConstPtr &msg)
-          {
-            fullMapUpdate(msg, subscription);
-          });
-      // subscription.is_self = robot_name == ros::this_node::getNamespace();
-      // ROS_INFO("subscription.is_self: %s", subscription.is_self ? "true" : "false");
-
-      // ROS_INFO("Subscribing to MAP updates topic: %s.",
-      //          map_updates_topic.c_str());
-      // subscription.map_updates_sub =
-      //     subscription_nh.subscribe<map_msgs::OccupancyGridUpdate>(
-      //         map_updates_topic, 50,
-      //         [this, &subscription](
-      //             const map_msgs::OccupancyGridUpdate::ConstPtr& msg) {
-      //           partialMapUpdate(msg, subscription);
-      //         });
     }
   }
 
+  // Return /tb3_0 for cases such as /tb3_0/map, /tb3_0/camera/rgb/image_raw and /tb3_0/tb3_0/map (this case is not used).
   std::string Traceback::robotNameFromTopic(const std::string &topic)
   {
-    return ros::names::parentNamespace(topic);
+    std::regex str_expr("(/tb3.*?)/.*");
+    std::smatch sm;
+    std::regex_match(topic, sm, str_expr);
+
+    if (sm.size() != 2)
+    {
+      ROS_ERROR("robotNameFromTopic returns unexpected result! Number of regex matches is %zu instead of the expected 2!", sm.size());
+      return "";
+    }
+
+    return sm[1];
   }
 
   bool Traceback::isRobotMapTopic(const ros::master::TopicInfo &topic)
   {
-    /* test whether topic is robot_map_topic_ */
-    std::string topic_namespace = ros::names::parentNamespace(topic.name);
-    bool is_map_topic =
-        ros::names::append(topic_namespace, robot_map_topic_) == topic.name;
-
     /* test whether topic contains *anywhere* robot namespace */
     auto pos = topic.name.find(robot_namespace_);
     bool contains_robot_namespace = pos != std::string::npos;
+    if (!contains_robot_namespace)
+    {
+      return false;
+    }
+
+    /* test whether topic is robot_map_topic_ */
+    std::string robot_namespace = robotNameFromTopic(topic.name);
+    bool is_map_topic =
+        ros::names::append(robot_namespace, robot_map_topic_) == topic.name;
+    if (!is_map_topic)
+    {
+      return false;
+    }
 
     /* we support only occupancy grids as maps */
     bool is_occupancy_grid = topic.datatype == "nav_msgs/OccupancyGrid";
 
-    return is_occupancy_grid && contains_robot_namespace && is_map_topic;
+    return is_occupancy_grid;
+  }
+
+  bool Traceback::isRobotCameraTopic(const ros::master::TopicInfo &topic)
+  {
+    /* test whether topic contains *anywhere* robot namespace */
+    auto pos = topic.name.find(robot_namespace_);
+    bool contains_robot_namespace = pos != std::string::npos;
+    if (!contains_robot_namespace)
+    {
+      return false;
+    }
+
+    /* test whether topic is robot_camera_topic_ */
+    std::string robot_namespace = robotNameFromTopic(topic.name);
+    bool is_camera_topic =
+        ros::names::append(robot_namespace, robot_camera_image_topic_) == topic.name;
+    if (!is_camera_topic)
+    {
+      return false;
+    }
+
+    /* we support only Image (not CompressedImage) */
+    bool is_image = topic.datatype == "sensor_msgs/Image";
+
+    return is_image;
   }
 
   void Traceback::executeUpdateTargetPoses()
@@ -291,6 +402,16 @@ namespace traceback
     }
   }
 
+  void Traceback::executeReceiveUpdatedCameraImage()
+  {
+    ros::Rate r(camera_image_update_rate_);
+    while (node_.ok())
+    {
+      receiveUpdatedCameraImage();
+      r.sleep();
+    }
+  }
+
   void Traceback::executePoseEstimation()
   {
     ros::Rate r(estimation_rate_);
@@ -309,6 +430,8 @@ namespace traceback
     ros::spinOnce();
     std::thread subscribing_thr([this]()
                                 { executeTopicSubscribing(); });
+    std::thread receive_camera_image_thr([this]()
+                                         { executeReceiveUpdatedCameraImage(); });
     std::thread estimation_thr([this]()
                                { executePoseEstimation(); });
     std::thread update_target_thr([this]()
@@ -316,6 +439,7 @@ namespace traceback
     ros::spin();
     update_target_thr.join();
     estimation_thr.join();
+    receive_camera_image_thr.join();
     subscribing_thr.join();
   }
 
