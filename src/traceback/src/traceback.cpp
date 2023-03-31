f968f9d3d3135f8bd243c3c3f6f653fa0d21a871
michaelfong2017 <michaelfong2017@gmail.com>
2023-04-01 00:05:00 +0800

    Save map to png successfully

--- src/traceback/src/traceback.cpp
+++ src/traceback/src/traceback.cpp
@@ -3520,49 +3520,33 @@ namespace traceback
     }
     std::string mapname = "map/" + current_time + "/" + map_name;
 
-    std::string mapdatafile = mapname + ".pgm";
+    std::string mapdatafile = mapname + ".png";
     ROS_INFO("Writing map occupancy data to %s", mapdatafile.c_str());
-    FILE *out = fopen(mapdatafile.c_str(), "w");
-    if (!out)
-    {
-      ROS_ERROR("Couldn't save map file to %s", mapdatafile.c_str());
-      return;
-    }
 
+    cv::Mat mat(map.info.height, map.info.width, CV_8UC1);
     for (unsigned int y = 0; y < map.info.height; y++)
     {
       for (unsigned int x = 0; x < map.info.width; x++)
       {
         unsigned int i = x + (map.info.height - y - 1) * map.info.width;
-        if (map.data[i] == 0)
-        { // occ [0,0.1)
-          fputc(254, out);
-        }
-        else if (map.data[i] == +100)
-        { // occ (0.65,1]
-          fputc(000, out);
+        if (map.data[i] == -1)
+        {
+          mat.at<uchar>(y, x) = static_cast<uchar>(205);
         }
         else
-        { // occ [0.1,0.65]
-          fputc(205, out);
+        {
+          mat.at<uchar>(y, x) = static_cast<uchar>((100 - map.data[i]) * 255 / 100);
         }
       }
     }
-
-    fclose(out);
+    if (!mat.empty())
+    {
+      cv::imwrite(mapdatafile, mat);
+    }
 
     std::string mapmetadatafile = mapname + ".yaml";
     ROS_INFO("Writing map occupancy data to %s", mapmetadatafile.c_str());
     FILE *yaml = fopen(mapmetadatafile.c_str(), "w");
-
-    /*
-  resolution: 0.100000
-  origin: [0.000000, 0.000000, 0.000000]
-  #
-  negate: 0
-  occupied_thresh: 0.65
-  free_thresh: 0.196
-     */
 
     tf2::Quaternion tf_q;
     tf2::fromMsg(map.info.origin.orientation, tf_q);
